73f114652e3205a406811c7119b823c60d610f6c
dodin2 <144305961+dodin2@users.noreply.github.com>
2025-08-03 23:05:07 +0900

    Update _165.java
    가장자리 범위를 따로 구하지말고 열 공간을 추가해서 구하기

--- _165.java
+++ _165.java
@@ -1,40 +1,23 @@
-import java.util.Scanner;
+package array;
 
 public class _165 {
 
 	public static void main(String[] args) {
 		
-		int arr[][] = new int[5][5];
-		arr[0][0] = arr[0][2] = arr[0][4] = 1;
-		
-		Scanner sc = new Scanner(System.in);
+		int arr[][] = new int[5][7];
+		arr[0][1] = arr[0][3] = arr[0][5] = 1;
 		
 		// 위행의 왼쪽과 오른쪽 값 구하는 식
-		// arr[i-1][j-1] + arr[i-1][j+1] 단, 두 번째 행 부터니까 i=1부터 진행, j도 
-		// arr[1,2,3,4][0]와, arr[1,2,3,4][4] 계산 따로 
+		// arr[i-1][j-1] + arr[i-1][j+1]
 		
-		for(int i=1; i<arr.length; i++) { // 가운데 계산
+		for(int i=1; i<arr.length; i++) { 
 			for(int j=1; j<arr[i].length-1; j++) {
 				arr[i][j] = arr[i-1][j-1] + arr[i-1][j+1];
 			}
 		}
 		
-		for(int i=1; i<arr.length; i++) { // 1,2,3,4 니까 4개니까 
-			arr[i][0] = arr[i-1][1];
-			// arr[1][0] = arr[0][1]
-			// arr[2][0] = arr[1][1]
-			// arr[3][0] = arr[2][1]
-			// arr[4][0] = arr[3][1]
-		}
-		
-		for(int i=1; i<arr.length; i++) { // 
-			arr[i][4] = arr[i-1][3];
-			// arr[1][4] = arr[0][3]
-		}
-		
-		
 		for(int i=0; i<arr.length; i++) {
-			for(int j=0; j<arr[i].length; j++) {
+			for(int j=1; j<arr[i].length-1; j++) {
 				System.out.print(arr[i][j] + " ");
 			}
 			System.out.println();
